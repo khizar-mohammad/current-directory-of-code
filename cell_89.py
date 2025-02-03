515ca14bd5afbf310272a28cf4fd0a6669cd758d
Khizar Muhammad Masuood <khizarmohammad30@gmail.com>
2025-02-03 21:43:08 +0100

    Initial commit

---
+++ cell_89.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[21]:
+
+
+import os
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Print the current working directory
+print("Current working directory:", current_directory)
+
